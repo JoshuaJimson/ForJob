8d4e4e3c4092a3734cbd031eb744daef80cb2d96
JoshuaJimosn <jimsonjoshua973@gmail.com>
2022-08-20 21:23:28 +0530

    Renamed files

--- src/test/java/org/example/AdditionTest.java
+++ src/test/java/org/example/AdditionTest.java
@@ -1,7 +1,7 @@
 package org.example;
 
-public class Addition {
-    public static void main(String args[]){
+public class AdditionTest {
+    public static void main(String[] args){
         System.out.println("First");
         System.out.println("Conflict");
     }
