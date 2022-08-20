794be643cda5aee865fa9d34328bd26b1dde0335
JoshuaJimson <96612244+JoshuaJimson@users.noreply.github.com>
2022-08-21 01:10:51 +0530

    Stash testing

--- src/test/java/org/example/AdditionTest.java
+++ src/test/java/org/example/AdditionTest.java
@@ -4,6 +4,7 @@ public class AdditionTest {
     public static void main(String[] args){
         System.out.println("First");
         System.out.println("Conflict");
+        System.out.println("Can stash Resolve Conflict from local it self");
     }
 
 }
